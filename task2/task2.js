bdd7f7034a7c96a646e45fbb1d744fa5a5a12059
dhanasekar <159462461+Dhanasekar490@users.noreply.github.com>
2024-05-10 17:04:08 +0530

    Add files via upload

---
+++ task2/task2.js
@@ -0,0 +1,12 @@
+//write a blog about object and it's internal representation in javascript
+
+
+
+
+/* javascript objects are dynamic data structures with key-values pairs,
+efficiently represented internally using hash tables.this representation 
+enablesfast property lookup and mainoulation making objects ideal for
+ organizing and managing data in javascript application . understanding 
+ this internal representation  empower developers to leverage objects effectively 
+ for builing powerful and efficient software solution
+ */
